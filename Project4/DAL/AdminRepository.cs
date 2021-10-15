45d1493926395bc76037e51dba55783c44b787af
Robert Larsen <robertlarsenn@gmail.com>
2021-10-15 13:41:55 +0200

    login start skjerm.

--- Project4/DAL/AdminRepository.cs
+++ Project4/DAL/AdminRepository.cs
@@ -62,10 +62,10 @@ namespace webAppBillett.DAL
                 bool ok = hash.SequenceEqual(Encoding.ASCII.GetBytes(brukeren.passord));
                 if (ok)
                 {
-                    HttpContext.Session.SetString("loggin", "erOk");
+    
                     return true;
                 }
-                HttpContext.Session.SetString("loggin", "");
+
                 return false;
             }
             catch (Exception e)
