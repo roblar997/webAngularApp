b3f6e6c1e8ae3fc56c8f545078214baabf9cac02
Robert Larsen <robertlarsenn@gmail.com>
2021-10-16 10:14:20 +0200

    ...

--- Project4/ClientApp/src/app/Models-typescript/Bruker.ts
+++ Project4/ClientApp/src/app/Models-typescript/Bruker.ts
@@ -1,5 +1,4 @@
 export class Bruker {
   brukernavn: string;
   passord: string;
-  salt: string;
 }
--- Project4/ClientApp/src/app/skjemaer/login/login.component.ts
+++ Project4/ClientApp/src/app/skjemaer/login/login.component.ts
@@ -2,6 +2,7 @@ import { HttpClient } from "@angular/common/http";
 import { Component, OnInit } from "@angular/core";
 import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
 import { Betaling } from "../../Models-typescript/Betaling";
+import { Bruker } from "../../Models-typescript/Bruker";
 
 @Component({
   selector: "app-skjemaer-login",
@@ -17,6 +18,18 @@ export class LoginComponent {
       brukernavn: ["", Validators.required],
       passord: ["", Validators.required],
 
+
+    });
+  }
+
+  login() {
+
+    const bruker = new Bruker();
+    bruker.brukernavn = this.Skjema.value.brukernavn;
+    bruker.passord = this.Skjema.value.passord;
+
+
+    this._http.post("admin/login", bruker).subscribe((res) => {
 
     });
   }
