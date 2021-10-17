db8915ba5eaac4c4d3e88309f70d925f43884f1c
Robert Larsen <robertlarsenn@gmail.com>
2021-10-17 15:26:03 +0200

    ...

--- Project4/ClientApp/src/app/app.component.ts
+++ Project4/ClientApp/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -9,10 +10,13 @@ export class AppComponent {
   title = 'app';
   erLoggetInn = undefined;
 
-  constructor( private _http: HttpClient) {
+  constructor( private _http: HttpClient, private router: Router) {
   }
 
   erInnlogget() {
+    if (this.erLoggetInn == true) {
+      this.router.navigate(['/lugar']);
+    }
     if (this.erLoggetInn != undefined) {
       return this.erLoggetInn;
     }
