bb057032742596468772258a9621534f65d3313a
Robert Larsen <robertlarsenn@gmail.com>
2021-10-17 15:23:07 +0200

    ...

--- Project4/ClientApp/src/app/logUt/logUt.component.ts
+++ Project4/ClientApp/src/app/logUt/logUt.component.ts
@@ -4,13 +4,14 @@ import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms'
 import { Billett } from "../Models-typescript/Billett";
 import { NgbModal } from "@ng-bootstrap/ng-bootstrap";
 import { Modal } from "../../modal/modal";
+import { Router } from "@angular/router";
 @Component({
   selector: "app-logut",
   templateUrl: "logut.component.html"
 })
 export class LogUtComponent {
 
-  constructor(private _http: HttpClient) {
+  constructor(private _http: HttpClient, private router : Router) {
 
   }
 
@@ -21,7 +22,7 @@ export class LogUtComponent {
   logUt() {
 
     this._http.get("admin/logUt").subscribe((res) => {
-    
+      this.router.navigate(['/']);
     });
   }
 }
